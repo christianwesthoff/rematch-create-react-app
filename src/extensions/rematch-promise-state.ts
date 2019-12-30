506ed6b1bf1a6c7c5e249dca87d9193829e3456e
Christian Westhoff <westhoffchristian1@gmail.com>
2019-12-30 17:46:14 +0100

    WIP

--- src/extensions/rematch-promise-state.ts
+++ src/extensions/rematch-promise-state.ts
@@ -1,5 +1,10 @@
 import { ExtractRematchDispatchersFromEffects, Model, Models, Plugin } from '@rematch/core'
 
+export type LoadingStateEntry = {
+	isLoading: boolean,
+	error: string|null
+}
+
 export interface LoadingConfig {
 	name?: string
 	whitelist?: string[]
@@ -9,45 +14,39 @@ export interface LoadingConfig {
 
 export interface LoadingState<M extends Models> {
 	loading: {
-		global: boolean,
-		models: { [modelName in keyof M]: boolean },
+		models: { [modelName in keyof M]: LoadingStateEntry },
 		effects: {
 			[modelName in keyof M]: {
-				[effectName in keyof ExtractRematchDispatchersFromEffects<M[modelName]['effects']>]: boolean
+				[effectName in keyof ExtractRematchDispatchersFromEffects<M[modelName]['effects']>]: LoadingStateEntry
 			}
 		},
 	}
 }
 
-const cntState:any = {
-	global: 0,
-	models: {},
-	effects: {},
-}
 
-const createLoadingAction = (converter: any, i: any) => (
+const createAction = (isLoading: boolean, error?: string) => (
 	state: any,
 	{ name, action }: any
 ) => {
-	cntState.global += i;
-	cntState.models[name] += i;
-	cntState.effects[name][action] += i;
-
 	return {
 		...state,
-		global: converter(cntState.global),
 		models: {
 			...state.models,
-			[name]: converter(cntState.models[name]),
+			[name]: { isLoading, error }
 		},
 		effects: {
 			...state.effects,
 			[name]: {
 				...state.effects[name],
-				[action]: converter(cntState.effects[name][action]),
+				[action]: { isLoading, error }
 			},
 		},
 	};
+}
+
+const cntState:any = {
+	models: {},
+	effects: {},
 }
 
 const validateConfig = (config:any) => {
@@ -79,22 +78,17 @@ export default (config: LoadingConfig = {}): Plugin => {
 
 	const loadingModelName = config.name || 'loading';
 
-	const converter =
-		config.asNumber === true ? (cnt: number) => cnt : (cnt: number) => cnt > 0
-
 	const loading: Model = {
 		name: loadingModelName,
 		reducers: {
-			hide: createLoadingAction(converter, -1),
-			show: createLoadingAction(converter, 1),
+			start: createAction(true),
+			success: createAction(false),
+			error: (state:any, payload:any) => createAction(false, payload.error)(state, payload)
 		},
 		state: {
 			...cntState,
 		},
 	}
-
-	cntState.global = 0
-	loading.state.global = converter(cntState.global);
 
 	return {
 		config: {
@@ -108,8 +102,8 @@ export default (config: LoadingConfig = {}): Plugin => {
 				return
 			}
 
-			cntState.models[name] = 0;
-			loading.state.models[name] = converter(cntState.models[name]);
+			cntState.models[name] = { error: null, isLoading: false };
+			loading.state.models[name] =  cntState.models[name];
 			loading.state.effects[name] = {};
 			const modelActions = (<any>this.dispatch)[name];
 
@@ -119,8 +113,8 @@ export default (config: LoadingConfig = {}): Plugin => {
 					return;
 				}
 
-				cntState.effects[name][action] = 0;
-				loading.state.effects[name][action] = converter(cntState.effects[name][action]);
+				cntState.effects[name][action] = { error: null, isLoading: false };
+				loading.state.effects[name][action] = cntState.effects[name][action];
 
 				const actionType = `${name}/${action}`;
 
@@ -140,14 +134,13 @@ export default (config: LoadingConfig = {}): Plugin => {
 				// create function with pre & post loading calls
 				const effectWrapper: any = async (...props:any) => {
 					try {
-						(<any>this.dispatch).loading.show({ name, action });
+						(<any>this.dispatch).loading.start({ name, action });
 						// waits for dispatch function to finish before calling "hide"
 						const effectResult = await origEffect(...props);
-						(<any>this.dispatch).loading.hide({ name, action });
+						(<any>this.dispatch).loading.success({ name, action });
 						return effectResult
 					} catch (error) {
-						(<any>this.dispatch).loading.hide({ name, action });
-						throw error
+						(<any>this.dispatch).loading.error({ name, action, error });
 					}
 				}
 
