feacaf611a07e1a1bb2d8e624ae078becc8d01ac
Christian Westhoff <westhoffchristian1@gmail.com>
2020-02-24 02:46:20 +0100

    WIP

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,6 @@
 import { init } from '@rematch/core'
 import { RematchRootDispatch, RematchRootState } from 'extensions/util'
 import * as models from 'models'
-import immerPlugin from '@rematch/immer'
 import selectPlugin from '@rematch/select'
 import queryPlugin from 'extensions/rematch-request'
 import subscribePlugin from 'extensions/rematch-subscribe'
@@ -11,7 +10,6 @@ import routerPlugin from 'extensions/rematch-router'
 export const store = init({
 	models,
 	plugins: [
-		immerPlugin(), 
 		selectPlugin(),
 		subscribePlugin(),
 		routerPlugin("router"),
