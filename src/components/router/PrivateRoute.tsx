d50d3c685f656a341dd686d13f5be6dde2787251
Christian Westhoff <westhoff@freightly.com>
2020-02-28 16:55:27 +0100

    WIP

--- src/components/router/PrivateRoute.tsx
+++ src/components/router/PrivateRoute.tsx
@@ -1,13 +1,12 @@
 import { useLocation } from 'react-router-dom';
 import React from 'react';
 import { RootState } from 'store';
-import { Claims } from 'models/userInfo';
 import ConditionalRoute from './ConditionalRoute';
 
 export type PRProps = {
 } & any;
 
-const PrivateRoute = ({component: Component, ...props }: PRProps) => {
+const PrivateRoute = (props: PRProps) => {
   const location = useLocation();
   return (
     <ConditionalRoute {...props} when={(state: RootState) => state.auth.isAuthorized} redirect="/login" meta={{ from: location }} />
