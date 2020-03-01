a9d5e328f8c6fed3c5255ca520d55a4a72eb04a4
Christian Westhoff <westhoffchristian1@gmail.com>
2020-03-01 16:09:58 +0100

    WIP

--- src/rematch/patch.ts
+++ src/rematch/patch.ts
@@ -1,7 +1,7 @@
 // monkey patch models; we switch parameter order of effects because it doesn't allow parameterless functions
 const patch = (models: any):void => Object.keys(models).forEach(key => {
 	const model = (models as any)[key];
-	if (typeof model.effects === "function") {
+	if (typeof model.effects === 'function') {
 		const effectsFunc = model.effects;
 		model.effects = (dispatch: any) => {
 			const effects = effectsFunc(dispatch);
@@ -11,7 +11,7 @@ const patch = (models: any):void => Object.keys(models).forEach(key => {
 			});
 			return effects;
 		}
-	} else if (typeof model.effects === "object") {
+	} else if (typeof model.effects === 'object') {
 		model.effects.forEach((key: string) => {
 			const effect = model.effects[key];
 			model.effects[key] = (payload: any, state: any) => effect(state, payload);
